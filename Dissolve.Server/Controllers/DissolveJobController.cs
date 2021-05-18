d56409a40f82ce7a8013e3170d28e5569e7a28e7
Toolf <nazar.smirnov.2001@gmail.com>
2021-05-18 23:20:13 +0300

    Add Server application

---
+++ Dissolve.Server/Controllers/DissolveJobController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Dissolve.Server.Services;
+using Dissolve.Shared.DTO;
+using Dissolve.Shared.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dissolve.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DissolveJobController : ControllerBase
+    {
+        [HttpPost]
+        [Route("split")]
+        public ActionResult<DissolveJobResponseDto> SplitJob([FromBody] DissolveJobRequestDto req)
+        {
+            var imageCount = req.ImageCount;
+            var workers = req.Workers;
+
+            try
+            {
+                var service = new DissolveJobService();
+                var splitJob = service.SplitJob(imageCount, workers);
+                var responseDto = new DissolveJobResponseDto()
+                {
+                    Work = splitJob.Select((x) => new IndividualWork()
+                    {
+                        Worker = x.Key,
+                        ImageEdit = x.Value
+                    }).ToList()
+                };
+                return Ok(responseDto);
+            }
+            catch (NoWorkersException e)
+            {
+                return BadRequest("List of workers should not be empty.");
+            }
+        }
+
+        [HttpPost]
+        [Route("totalTime")]
+        public ActionResult<int> TotalJobTime([FromBody] DissolveJobRequestDto req)
+        {
+            var imageCount = req.ImageCount;
+            var workers = req.Workers;
+
+            try
+            {
+                var service = new DissolveJobService();
+                var totalTime = service.TotalJobTime(imageCount, workers);
+                return Ok(totalTime);
+            }
+            catch (NoWorkersException e)
+            {
+                return BadRequest("List of workers should not be empty.");
+            }
+        }
+    }
+}
