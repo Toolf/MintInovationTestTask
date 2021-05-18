0154b43e9e6fa19e9edbfd29384693ef52ff5726
Toolf <nazar.smirnov.2001@gmail.com>
2021-05-18 23:22:35 +0300

    Add client side

---
+++ Dissolve.Client/Services/IDissolveJobService.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dissolve.Shared.Entities;
+
+namespace Dissolve.Client.Services
+{
+    public interface IDissolveJobService
+    {
+        Task<Dictionary<ImageEditWorker, int>> DissolveJob(int imageCount, List<ImageEditWorker> workers);
+
+        Task<int> TotalTimeJob(int imageCount, List<ImageEditWorker> workers);
+    }
+}
